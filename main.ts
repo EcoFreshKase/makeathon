054966cde4ac9af46764d4e19e9abb76da24bf9a
Alexander <106274860+EcoFreshKase@users.noreply.github.com>
2024-04-16 13:05:08 +0200

    Add class to handle the play of music

--- main.ts
+++ main.ts
@@ -1,17 +1,49 @@
+enum emptyNote {
+    Pause = 0
+}
+
+type PlayableNote = emptyNote | Note
+
+class MusicPlayer {
+    curNote: number;
+    notes: PlayableNote[];
+
+    constructor(notes: PlayableNote[]) {
+        this.curNote = 0;
+        this.notes = notes;
+    }
+
+    play() {
+        if (this.curNote > this.notes.length) {
+            this.playPause();
+        }
+        music.playTone(this.notes[this.curNote], music.beat(BeatFraction.Half));
+        this.curNote = (this.curNote + 2) % this.notes.length;
+    }
+
+    playPause() {
+        music.playTone(0, music.beat(BeatFraction.Whole));
+    }
+}
+
+
+let musicPlayer: MusicPlayer = new MusicPlayer([Note.E, Note.E, emptyNote.Pause, Note.E, emptyNote.Pause, Note.C, Note.E])
+
 basic.forever(function () {
-    music.playTone(Note.E, music.beat(BeatFraction.Quarter))
-    music.playTone(Note.E, music.beat(BeatFraction.Quarter))
-    music.playTone(0, music.beat(BeatFraction.Quarter))
-    music.playTone(Note.E, music.beat(BeatFraction.Quarter))
+    musicPlayer.play();
+    // music.playTone(Note.E, music.beat(BeatFraction.Quarter))
+    // music.playTone(Note.E, music.beat(BeatFraction.Quarter))
+    // music.playTone(0, music.beat(BeatFraction.Quarter))
+    // music.playTone(Note.E, music.beat(BeatFraction.Quarter))
 
-    music.playTone(0, music.beat(BeatFraction.Quarter))
-    music.playTone(Note.C, music.beat(BeatFraction.Quarter))
-    music.playTone(Note.E, music.beat(BeatFraction.Half))
+    // music.playTone(0, music.beat(BeatFraction.Quarter))
+    // music.playTone(Note.C, music.beat(BeatFraction.Quarter))
+    // music.playTone(Note.E, music.beat(BeatFraction.Half))
 
-    music.playTone(Note.G, music.beat(BeatFraction.Half))
-    music.playTone(0, music.beat(BeatFraction.Half))
+    // music.playTone(Note.G, music.beat(BeatFraction.Half))
+    // music.playTone(0, music.beat(BeatFraction.Half))
 
-    music.playTone(Note.C, music.beat(BeatFraction.Half))
+    // music.playTone(Note.C, music.beat(BeatFraction.Half))
 
-    music.playTone(0, music.beat(BeatFraction.Whole))
+    // music.playTone(0, music.beat(BeatFraction.Whole))
 })
